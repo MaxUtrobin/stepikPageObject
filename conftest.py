3d1c3abed8ca0998f281c0add1f74ae5819283b4
MaxUtrobin <ma.utrobin@gmail.com>
2021-11-22 13:30:36 +0200

    first test

--- conftest.py
+++ conftest.py
@@ -20,7 +20,6 @@ def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(options=options)
     # if browser_name == "chrome":
     #     options = Options()
@@ -35,5 +34,4 @@ def browser(request):
     # else:
     #     raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,4 @@
-
-
-class BasePage():
+class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
